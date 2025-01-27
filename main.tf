2bd17be521288fa26ecfe420031403fbe13c8884
gthri2k <82557823+gthri2k@users.noreply.github.com>
2025-01-27 21:53:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"  
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example-terraform-jenkins"
-  acl    = "private"
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
 }
